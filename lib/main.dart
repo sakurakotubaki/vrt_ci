c29350907d00c904a434182ea3f5748c60c09930
sakurakotubaki <gitonly543@gmail.com>
2026-04-07 13:13:40 +0900

    wip: develop

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('VRT CI'),
       ),
+      body: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset('assets/images/Dash.png'),
+          ),
     );
   }
 }
